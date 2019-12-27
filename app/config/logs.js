1d5ab39f77d83b9b871c8382e1d88eb33b712477
Alameddin CELIK <alcelik@netas.com.tr>
2019-12-27 15:29:03 +0300

    API, DB and Redis Log Methods created

--- app/config/logs.js
+++ app/config/logs.js
@@ -2,12 +2,15 @@ const {ErrorSchema} = require('./logConfig/logSchema');
 const fs = require('fs');
 
 const error_log = new ErrorSchema("error","error_logs");
+const api_log = new ErrorSchema("API","api_logs");
+const db_logs = new ErrorSchema("DB","db_logs");
+const redis_logs = new ErrorSchema("Redis","redis_logs");
 
 const logFactory = (data, error_log) => {
     const d = new Date();
     const filename = error_log.filename + d.getDay() + '-' + d.getDay() + '-' + d.getDay() + '.log';
 
-    console.log(error_log.filename + "      " + data);
+    console.log(error_log.name + "      " + data);
     if (fs.existsSync(filename)) {
         fs.appendFile(filename, data, function (err) {
             if (err) throw err;
@@ -22,5 +25,8 @@ const logFactory = (data, error_log) => {
 }
 
 module.exports = {
-    error_log: (input) => logFactory(input,error_log),
+    error_logs: (input) => logFactory(input,error_log),
+    api_logs: (input) => logFactory(input,api_log),
+    db_logs: (input) => logFactory(input,db_logs),
+    redis_logs: (input) => logFactory(input,redis_logs),
 }
